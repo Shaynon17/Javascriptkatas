70e3760e5ba03cd67847b8aeaea7ce30c98b4a53
shaynon17 <shannon.wlmiller@yahoo.com>
2022-06-01 10:51:31 -0500

    first commit

---
+++ tdd.js
@@ -0,0 +1,53 @@
+function add(x, y) {
+    return x + y;
+}
+
+
+test('add will sum together two numbers', function (assert) {
+    let result = add(1, 2)
+    assert.equal(result, 3);
+});
+
+function sub(x, y) {
+    return x - y;
+}
+
+test('sub will subtract a from b', function (assert) {
+    let result = sub(4, 2)
+    assert.equal(result, 2);
+});
+
+test('sub will subtract a from b with different numbers', function (assert) {
+    let result = sub(15, 2)
+    assert.equal(result, 13);
+});
+
+function helloThere(name) {
+    return "hello " + name;
+}
+
+test('helloThere will prepend hello to the name', function (assert) {
+    let result = helloThere("steve")
+    assert.equal(result, "hello steve");
+});
+
+test('helloThere will prepend hello to any name', function (assert) {
+    let result = helloThere("rooster")
+    assert.equal(result, "hello rooster");
+});
+
+// test driven development  -> watch youtube/read articles
+// RED -> test fails for the right reason (test is complete)
+// GREEN -> you make test pass by fixing the code
+// REFACTOR -> clean up the code
+
+// cd  -> change directory 
+// cd Coding\Javascript
+
+// kata practice
+
+
+test('fizzbuzz(1) will return 1', function (assert) {
+    let result = fizzbuzz(1)
+    assert.equal(result, 1);
+});
---
+++ test.js
@@ -0,0 +1,63 @@
+let aString = "test";
+let y = 5;
+let z = false;
+let name = "shannon";
+let space = " "
+
+// console.log("the value of name is: " + name);
+// console.log(y + space + aString);
+// console.log(z)
+
+
+function double(x) {
+    console.log("x is: " + x);
+    return x * 2;
+}
+
+function add(x, y) {
+    console.log("x is: " + x);
+    console.log("y is: " + y);
+    return x + y + 10;
+}
+
+// //console.log("horse horse")
+// //let sum = add(4, 5); // -> add(x = 4, y = 5)
+// // let dubs = double(8); // -> double(x = 8)
+// // console.log("sum is:" + sum);
+// console.log("double is:" + dubs);
+
+function sayhello(n) {
+    console.log("hello " + n);
+    return (n)
+}
+
+let astring = "steve";
+sayhello(name)
+sayhello(astring)
+let n = "amber"
+sayhello(n)
+
+// Do NOT delete or touch what is above this
+
+let a = 7
+let b = 6
+let c = 2
+
+
+
+function test(x1, y1) {
+    return x1 + y1
+}
+
+console.log(test(a, 7))
+console.log(test(8, 17))
+
+
+
+
+ /* console.log (a + b)
+ console.log (a - b)
+ console.log (c)
+ console.log (a % c)
+ console.log (Math.random()*100)
+console.log (b / c) */
