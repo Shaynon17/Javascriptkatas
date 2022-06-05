00e755355a0cdda382dfc7cb7e8044976a186bd6
shaynon17 <shannon.wlmiller@yahoo.com>
2022-06-05 13:40:28 -0500

    practicing fizzbuzz Co-authored-by: Steve Shogren <steveshogren@users.noreply.github.com>

--- scratch.js
+++ scratch.js
@@ -1,1 +1,23 @@
-console.log('hS')
+//if 3 then fizz
+//if 5 then buzz
+// if 15 then fizzbuzz
+
+function fizzBuzzConverter(number) {
+    if (isDivisibleBy(number, 5) && isDivisibleBy(number, 3)) {
+        return "fizzbuzz"
+    } else if (isDivisibleBy(number, 3)) {
+        return "fizz"
+    } else if (isDivisibleBy(number, 5)) {
+        return "buzz"
+    } else {
+        return number
+    }
+}
+
+function isDivisibleBy(larger, small) {
+    return (larger % small === 0)
+}
+
+console.log(fizzBuzzConverter(9))
+console.log(fizzBuzzConverter(10))
+console.log(fizzBuzzConverter(45))
